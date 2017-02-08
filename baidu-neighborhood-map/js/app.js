1f4bd8a32f8cbb5fddefb7fe8131b5826db1c8f6
xueyan123321 <1152991535@qq.com>
2017-02-08 15:22:54 +0800

    add the WeiBo

--- baidu-neighborhood-map/js/app.js
+++ baidu-neighborhood-map/js/app.js
@@ -1,14 +1,31 @@
 var initialMarkers=[
 {
-	"title":"东莞社区",
+	"title":"东冠社区",
+	'lat':120.170921,
+	'lng':30.174058
 },
-// {
-// 	"title":"浙江工业大学屏峰校区"
-// },
-// {
-// 	"title":"杭州火车站"
-// }
+{
+	"title":"浙江工业大学屏峰校区",
+	"lat":120.044132,
+	"lng":30.231677
+},
+{
+	"title":"杭州火车站",
+	"lat":120.188008,
+	"lng":30.249638
+}
 ];
+
+var Marker=function(data){
+	var self=this;
+	self.title=data.title;
+	self.lat=data.lat;
+	self.lng=data.lng;
+	self.content='';
+	self.visible=ko.observable(true);
+
+	var =
+}
 
 var ViewModel=function(){
 	var self=this;
@@ -20,9 +37,22 @@ var ViewModel=function(){
 	});
 
 	self.filterValue=ko.observable('');
-	// self.visible=ko.computed(function(){
+	self.markFilter=ko.computed(function(){
+		return ko.utils.arrayFilter(self.markerList(),function(markerItem){
+			if(!self.filterValue()||(markerItem.title.indexOf(self.filterValue())>-1))
+			{
+				markerItem.visible(true);
+				return true;
+			}
+			else
+			{
+				markerItem.visible(false);
+				return false;
+			}
 
-	// },self);
+		})
+
+	},self);
 	self.toggleList=function(){
 		document.getElementById('list').classList.toggle("list-toggle");
 		console.log(self.markerList().length>1);
